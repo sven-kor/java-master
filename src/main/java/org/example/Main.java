e1a15dd01a1dd2070dde038adad2b26582846078
Hyungjun Kim <hjhj919@naver.com>
2025-04-23 01:14:58 +0900

    oomeTest

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,27 @@
 package org.example;
+import  java.util.*;
+
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        List<Set<Integer>> allLottos = new ArrayList<>();
+
+        while (true) {
+            Set<Integer> lotto = generateLotto();
+            allLottos.add(lotto); // 계속 리스트에 추가하면서 메모리 점점 사용
+            if (allLottos.size() % 100000 == 0) {
+                System.out.println("Generated: " + allLottos.size() + " lotto sets");
+            }
+        }
+    }
+
+    public static Set<Integer> generateLotto() {
+        Random random = new Random();
+        Set<Integer> lotto = new HashSet<>();
+        while (lotto.size() < 6) {
+            int number = random.nextInt(45) + 1; // 1 ~ 45
+            lotto.add(number);
+        }
+        return lotto;
     }
 }
